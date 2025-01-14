e5d6fe0d5f67a0aebb6cf2611e7ed387879bf53a
Sev4ik74k <sev4ikrnb@gmail.com>
2025-01-14 11:21:30 +0100

    homework 21

--- main.js
+++ main.js
@@ -1,65 +1,56 @@
-const todosWrapper = $(".js--todos-wrapper");
-const modalTaskText = $("#modalTaskText");
+"use strict";
 
-const loadTodos = () => {
-    const todos = JSON.parse(localStorage.getItem("todos")) || [];
-    todosWrapper.empty();
-    todos.forEach(todo => addTodoToDOM(todo.text, todo.completed));
+var todosWrapper = $(".js--todos-wrapper");
+var modalTaskText = $("#modalTaskText");
+var loadTodos = function loadTodos() {
+  var todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todosWrapper.empty();
+  todos.forEach(function (todo) {
+    return addTodoToDOM(todo.text, todo.completed);
+  });
 };
 
-const saveTodos = () => {
-    const todos = [];
-    $(".list-group-item").each(function () {
-        const text = $(this).find(".todo-item__description").text();
-        const completed = $(this).hasClass("todo-item--checked");
-        todos.push({ text, completed });
+var saveTodos = function saveTodos() {
+  var todos = [];
+  $(".list-group-item").each(function () {
+    var text = $(this).find(".todo-item__description").text();
+    var completed = $(this).hasClass("todo-item--checked");
+    todos.push({
+      text: text,
+      completed: completed
     });
-    localStorage.setItem("todos", JSON.stringify(todos));
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
-
-const addTodo = (text, completed = false) => {
-    const todoItem = $(`
-        <li class="list-group-item ${completed ? "todo-item--checked" : ""}">
-            <input type="checkbox" ${completed ? "checked" : ""} class="form-check-input me-2">
-            <span class="todo-item__description">${text}</span>
-            <div>
-                <button class="btn btn-info btn-sm me-2 todo-item__view">Переглянути</button>
-                <button class="btn btn-danger btn-sm todo-item__delete">Видалити</button>
-            </div>
-        </li>
-    `);
-    todosWrapper.append(todoItem);
+var addTodo = function addTodo(text) {
+  var completed = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+  var todoItem = $("\n        <li class=\"list-group-item ".concat(completed ? "todo-item--checked" : "", "\">\n            <input type=\"checkbox\" ").concat(completed ? "checked" : "", " class=\"form-check-input me-2\">\n            <span class=\"todo-item__description\">").concat(text, "</span>\n            <div>\n                <button class=\"btn btn-info btn-sm me-2 todo-item__view\">\u041F\u0435\u0440\u0435\u0433\u043B\u044F\u043D\u0443\u0442\u0438</button>\n                <button class=\"btn btn-danger btn-sm todo-item__delete\">\u0412\u0438\u0434\u0430\u043B\u0438\u0442\u0438</button>\n            </div>\n        </li>\n    "));
+  todosWrapper.append(todoItem);
 };
-
 $(".js--form").on("submit", function (e) {
-    e.preventDefault();
-    const input = $(".js--form__input");
-    const value = input.val().trim();
-    if (value) {
-        addTodo(value);
-        saveTodos();
-        input.val("");
-    }
+  e.preventDefault();
+  var input = $(".js--form__input");
+  var value = input.val().trim();
+  if (value) {
+    addTodo(value);
+    saveTodos();
+    input.val("");
+  }
 });
-
 todosWrapper.on("change", "input[type='checkbox']", function () {
-    $(this).closest(".list-group-item").toggleClass("todo-item--checked");
-    saveTodos();
+  $(this).closest(".list-group-item").toggleClass("todo-item--checked");
+  saveTodos();
 });
-
 todosWrapper.on("click", ".todo-item__delete", function () {
-    $(this).closest(".list-group-item").remove();
-    saveTodos();
+  $(this).closest(".list-group-item").remove();
+  saveTodos();
 });
-
 todosWrapper.on("click", ".todo-item__view", function () {
-    const text = $(this).closest(".list-group-item").find(".todo-item__description").text();
-    modalTaskText.text(text);
-    $("#todoModal").addClass("show").css("display", "block");
+  var text = $(this).closest(".list-group-item").find(".todo-item__description").text();
+  modalTaskText.text(text);
+  $("#todoModal").addClass("show").css("display", "block");
 });
-
 $(".modal .btn-close, .modal .btn-secondary").on("click", function () {
-    $("#todoModal").removeClass("show").css("display", "none");
+  $("#todoModal").removeClass("show").css("display", "none");
 });
-
 loadTodos();
